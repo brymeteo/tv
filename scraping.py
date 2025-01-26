4810eb9aea7a928849d0220d735bd164700cd822
brycinema <46980007+brymeteo@users.noreply.github.com>
2025-01-26 04:38:43 +0100

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -65,6 +65,7 @@ def scrape_epg(url, canale_info, data_odierna):
 
     # Variabile per tenere traccia dell'orario di inizio del programma precedente
     orario_inizio_precedente = None
+    ora_corrente = datetime.datetime.now()
 
     for i, programma in enumerate(programmi):
         # Estrai i dettagli del programma
@@ -83,8 +84,11 @@ def scrape_epg(url, canale_info, data_odierna):
         # Combina la data odierna con l'orario di inizio
         orario_inizio_completo = f"{data_odierna}T{orario_inizio}:00.000000Z"
 
-        # Sottrai un'ora all'orario di inizio
-        orario_inizio_completo = (datetime.datetime.strptime(orario_inizio_completo, "%Y-%m-%dT%H:%M:%S.%fZ") - datetime.timedelta(hours=1)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        # Se il programma è in corso, adatta l'orario di inizio all'ora corrente
+        orario_inizio_dt = datetime.datetime.strptime(orario_inizio_completo, "%Y-%m-%dT%H:%M:%S.%fZ")
+        if ora_corrente >= orario_inizio_dt:
+            # Se il programma è già in corso, inizia dall'ora corrente
+            orario_inizio_completo = ora_corrente.strftime("%Y-%m-%dT%H:%M:%S.000000Z")
 
         # Trova l'URL del poster
         poster_img = programma.find('img')
@@ -121,9 +125,6 @@ def scrape_epg(url, canale_info, data_odierna):
 
             # Sottrai un'ora dall'orario di fine
             orario_fine_ultimo = orario_fine_ultimo - datetime.timedelta(hours=1)
-
-            # Se l'ultimo programma è davvero l'ultimo, aumenta di un'ora l'orario di fine
-            orario_fine_ultimo += datetime.timedelta(hours=1)
 
             # Se l'orario di fine è successivo alla mezzanotte, aggiorniamo la data
             if orario_fine_ultimo.day != orario_inizio_ultimo.day:  
