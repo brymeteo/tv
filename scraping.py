0055986400cde52cef9212fde6cfa7e92617ff9a
brycinema <46980007+brymeteo@users.noreply.github.com>
2025-01-27 03:46:39 +0100

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -83,8 +83,8 @@ def scrape_epg(url, canale_info, data_odierna):
         # Combina la data odierna con l'orario di inizio
         orario_inizio_completo = f"{data_odierna}T{orario_inizio}:00.000000Z"
 
-        # Rimuovi la sottrazione di un'ora all'orario di inizio
-        orario_inizio_completo = datetime.datetime.strptime(orario_inizio_completo, "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        # Sottrai un'ora all'orario di inizio
+        orario_inizio_completo = (datetime.datetime.strptime(orario_inizio_completo, "%Y-%m-%dT%H:%M:%S.%fZ") - datetime.timedelta(hours=1)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
         # Trova l'URL del poster
         poster_img = programma.find('img')
@@ -96,7 +96,7 @@ def scrape_epg(url, canale_info, data_odierna):
 
         # Calcola l'orario di fine basandoti sull'inizio del prossimo programma
         if orario_inizio_precedente:
-            dati_programmi[-1]['end'] = datetime.datetime.strptime(f"{data_odierna}T{orario_inizio}:00.000000Z", "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            dati_programmi[-1]['end'] = (datetime.datetime.strptime(f"{data_odierna}T{orario_inizio}:00.000000Z", "%Y-%m-%dT%H:%M:%S.%fZ") - datetime.timedelta(hours=1)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
         # Crea l'oggetto per il programma corrente
         programma_data = {
@@ -116,11 +116,14 @@ def scrape_epg(url, canale_info, data_odierna):
     if dati_programmi:
         ultimo_programma = dati_programmi[-1]
         try:
-            # Estrai l'orario di inizio dell'ultimo programma
             orario_inizio_ultimo = datetime.datetime.strptime(ultimo_programma['start'].split("T")[1][:5], "%H:%M")
-
-            # Calcola l'orario di fine aggiungendo 1 ora
             orario_fine_ultimo = orario_inizio_ultimo + datetime.timedelta(hours=1)  # Aggiungi 1 ora all'orario di inizio
+
+            # Sottrai un'ora dall'orario di fine
+            orario_fine_ultimo = orario_fine_ultimo - datetime.timedelta(hours=1)
+
+            # Se l'ultimo programma è davvero l'ultimo, aumenta di un'ora l'orario di fine
+            orario_fine_ultimo += datetime.timedelta(hours=1)
 
             # Se l'orario di fine è successivo alla mezzanotte, aggiorniamo la data
             if orario_fine_ultimo.day != orario_inizio_ultimo.day:  
